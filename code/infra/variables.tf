44fa59009424527729c20c22683cd21922e1a112
Marvin Buss <marvin.buss@gmail.com>
2025-10-15 00:50:51 +0200

    Add infra baseline

---
+++ code/infra/variables.tf
@@ -0,0 +1,177 @@
+# General variables
+variable "location" {
+  description = "Specifies the location for all Azure resources."
+  type        = string
+  sensitive   = false
+}
+
+variable "environment" {
+  description = "Specifies the environment of the deployment."
+  type        = string
+  sensitive   = false
+  default     = "dev"
+  validation {
+    condition     = contains(["int", "dev", "tst", "qa", "uat", "prd"], var.environment)
+    error_message = "Please use an allowed value: \"int\", \"dev\", \"tst\", \"qa\", \"uat\" or \"prd\"."
+  }
+}
+
+variable "prefix" {
+  description = "Specifies the prefix for all resources created in this deployment."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(var.prefix) >= 2 && length(var.prefix) <= 10
+    error_message = "Please specify a prefix with more than two and less than 10 characters."
+  }
+}
+
+variable "tags" {
+  description = "Specifies the tags that you want to apply to all resources."
+  type        = map(string)
+  sensitive   = false
+  default     = {}
+}
+
+# Service variables
+variable "container_image_reference" {
+  description = "Specifies the container image reference used in Azure Container Jobs."
+  type        = string
+  sensitive   = true
+  validation {
+    condition     = length(var.container_image_reference) > 2
+    error_message = "Please specify a valid container reference."
+  }
+}
+
+# HA/DR variables
+variable "zone_redundancy_enabled" {
+  description = "Specifies whether zone-redundancy should be enabled for all resources."
+  type        = bool
+  sensitive   = false
+  nullable    = false
+  default     = true
+}
+
+# Logging and monitoring variables
+variable "log_analytics_workspace_id" {
+  description = "Specifies the resource ID of the log analytics workspace used for collecting logs."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.log_analytics_workspace_id)) == 9
+    error_message = "Please specify a valid resource ID."
+  }
+}
+
+# Identity variables
+variable "service_principal_name_terraform_plan" {
+  description = "Specifies the name of the service principal used for the Terraform plan in PRs."
+  type        = string
+  sensitive   = false
+  default     = ""
+  validation {
+    condition     = var.service_principal_name_terraform_plan == "" || length(var.service_principal_name_terraform_plan) >= 2
+    error_message = "Please specify a valid name."
+  }
+}
+
+# Network variables
+variable "vnet_id" {
+  description = "Specifies the resource ID of the Vnet used for the Data Landing Zone."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.vnet_id)) == 9
+    error_message = "Please specify a valid resource ID."
+  }
+}
+
+variable "nsg_id" {
+  description = "Specifies the resource ID of the default network security group for the Azure Function."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.nsg_id)) == 9
+    error_message = "Please specify a valid resource ID."
+  }
+}
+
+variable "route_table_id" {
+  description = "Specifies the resource ID of the default route table for the Azure Function."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.route_table_id)) == 9
+    error_message = "Please specify a valid resource ID."
+  }
+}
+
+variable "subnet_cidr_container_app" {
+  description = "Specifies the subnet cidr range for teh container app subnet."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.subnet_cidr_container_app)) == 2
+    error_message = "Please specify a valid subnet cidr range."
+  }
+}
+
+variable "subnet_cidr_private_endpoints" {
+  description = "Specifies the subnet cidr range for private endpoints."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.subnet_cidr_private_endpoints)) == 2
+    error_message = "Please specify a valid subnet cidr range."
+  }
+}
+
+variable "connectivity_delay_in_seconds" {
+  description = "Specifies the delay in seconds after the private endpoint deployment (required for the DNS automation via Policies)."
+  type        = number
+  sensitive   = false
+  nullable    = false
+  default     = 120
+  validation {
+    condition     = var.connectivity_delay_in_seconds >= 0
+    error_message = "Please specify a valid non-negative number."
+  }
+}
+
+# DNS variables
+variable "private_dns_zone_id_vault" {
+  description = "Specifies the resource ID of the private DNS zone for Azure Key Vault. Not required if DNS A-records get created via Azure Policy."
+  type        = string
+  sensitive   = false
+  default     = ""
+  validation {
+    condition     = var.private_dns_zone_id_vault == "" || (length(split("/", var.private_dns_zone_id_vault)) == 9 && endswith(var.private_dns_zone_id_vault, "privatelink.vaultcore.azure.net"))
+    error_message = "Please specify a valid resource ID for the private DNS Zone."
+  }
+}
+
+# Customer-managed key variables
+variable "customer_managed_key" {
+  description = "Specifies the customer managed key configurations."
+  type = object({
+    key_vault_id                     = string,
+    key_vault_key_id                 = string,
+    key_vault_key_versionless_id     = string,
+    user_assigned_identity_id        = string,
+    user_assigned_identity_client_id = string,
+  })
+  sensitive = false
+  nullable  = true
+  default   = null
+  validation {
+    condition = alltrue([
+      var.customer_managed_key == null || length(split("/", try(var.customer_managed_key.key_vault_id, ""))) == 9,
+      var.customer_managed_key == null || startswith(try(var.customer_managed_key.key_vault_key_id, ""), "https://"),
+      var.customer_managed_key == null || startswith(try(var.customer_managed_key.key_vault_key_versionless_id, ""), "https://"),
+      var.customer_managed_key == null || length(split("/", try(var.customer_managed_key.user_assigned_identity_id, ""))) == 9,
+      var.customer_managed_key == null || length(try(var.customer_managed_key.user_assigned_identity_client_id, "")) >= 2,
+    ])
+    error_message = "Please specify a valid resource ID."
+  }
+}
