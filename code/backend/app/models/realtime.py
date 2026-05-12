d5346d34553421addb07dd6024deaee095c24563
Marvin Buss <marvin.buss@gmail.com>
2026-05-13 01:52:03 +0200

    Update models

--- code/backend/app/models/realtime.py
+++ code/backend/app/models/realtime.py
@@ -1,7 +1,9 @@
 from azure.communication.callautomation import CallAutomationClient
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class UserSessionContext(BaseModel):
     acs_client: CallAutomationClient
     call_connection_id: str
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
