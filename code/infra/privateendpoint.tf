34130e0227bb7e5bec22d7a3797bf38c9f8e761f
Marvin Buss <marvin.buss@gmail.com>
2026-06-07 23:52:09 +0200

    Update PE

--- code/infra/privateendpoint.tf
+++ code/infra/privateendpoint.tf
@@ -1,22 +1,21 @@
 resource "azurerm_private_endpoint" "private_endpoint" {
-  name                = "${local.prefix}-oai-tst-pe"
+  name                = "${local.prefix}-search-tst-pe"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = var.location
   tags                = var.tags
 
-  custom_network_interface_name = "${local.prefix}-oai-tst-nic"
+  custom_network_interface_name = "${local.prefix}-search-tst-nic"
   private_service_connection {
-    name                           = "${local.prefix}-oai-tst-svc"
-    is_manual_connection           = true
-    private_connection_resource_id = "/subscriptions/1fdab118-1638-419a-8b12-06c9543714a0/resourceGroups/aml-test-rg/providers/Microsoft.CognitiveServices/accounts/aoai-mabuss-cs001"
-    request_message                = "Private Endpoint Connection Request from Data Landing Zone Stamp Application with prefix: ${local.prefix}"
-    subresource_names              = ["account"]
+    name                           = "${local.prefix}-search-tst-svc"
+    is_manual_connection           = false
+    private_connection_resource_id = "/subscriptions/1fdab118-1638-419a-8b12-06c9543714a0/resourceGroups/fnf-test/providers/Microsoft.Search/searchServices/fnf-search"
+    subresource_names              = ["searchService"]
   }
   subnet_id = azapi_resource.subnet_private_endpoints.id
   private_dns_zone_group {
-    name = "${local.prefix}-oai-tst-arecord"
+    name = "${local.prefix}-search-tst-arecord"
     private_dns_zone_ids = [
-      var.private_dns_zone_id_open_ai
+      "/subscriptions/e82c5267-9dc4-4f45-ac13-abdd5e130d27/resourceGroups/ptt-dev-privatedns-rg/providers/Microsoft.Network/privateDnsZones/privatelink.search.windows.net"
     ]
   }
 
