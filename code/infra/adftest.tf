1d1b64a96da8ddb7b48770f9f5312a6430eee3d8
Marvin Buss <marvin.buss@gmail.com>
2026-06-08 11:33:46 +0200

    Update linked service

--- code/infra/adftest.tf
+++ code/infra/adftest.tf
@@ -85,19 +85,18 @@ resource "azapi_resource" "data_factory_linked_service_azure_function" {
     properties = {
       annotations = []
       description = "Linked Service for Azure Function used in Terraform testing"
+      connectVia = {
+        referenceName = azurerm_data_factory_integration_runtime_azure.data_factory_integration_runtime_azure.name
+        type          = "IntegrationRuntimeReference"
+      }
       type        = "AzureFunction"
       typeProperties = {
         functionAppUrl = "https://myfunctionapp.azurewebsites.net/"
         authentication = "MSI"
         resourceId     = "api:847393e5-9c8b-4c83-9a1c-8d0b1e7cbbf2"
-        key            = "foo"
         functionKey = {
           type  = "SecureString"
           value = "foo"
-        }
-        connectVia = {
-          referenceName = azurerm_data_factory_integration_runtime_azure.data_factory_integration_runtime_azure.name
-          type          = "IntegrationRuntimeReference"
         }
       }
     }
